7573be53e05c0229bfbbab4a6dcd2020f125ee81
Rakesh <justsineup4321@gmail.com>
2024-10-12 23:03:34 +0530

    Add a hardcore notes for showing under 'Your Notes' using 'useContext'

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import noteContext from "../context/notes/noteContext"
 
 const Home = () => {
+
+  const context = useContext(noteContext)
+  const {notes, setNotes} = context;
+
   return (
     <div>
       <div className='container my-3'>
@@ -25,6 +30,10 @@ const Home = () => {
 
       <div className='container'>
       <h2>Your Notes</h2>
+      {/* 👉 Add this */}
+      {notes.map((note) => {
+        return note.title;
+      })}
       </div>
     </div>
   )
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -1,9 +1,31 @@
+import { useState } from "react";
 import NoteContext from "./noteContext";
 
 const NoteState = (props) => {
+    const notesInitial = [              // This is bring from 'Fetch All Notes'...
+        {
+          "_id": "67058f9ac50bd0576ca7792b",
+          "user": "6704f89a7397abc96aa1d203",
+          "title": "My first title",
+          "description": "This is first description",
+          "tag": "personal",
+          "date": "2024-10-08T20:01:30.902Z",
+          "__v": 0
+        },
+        {
+          "_id": "14758f9ac5udt05760u9794f",
+          "user": "9364f89a7935ahd56aa1d028",
+          "title": "My second title",
+          "description": "This is second description",
+          "tag": "public",
+          "date": "2024-10-08T20:01:30.902Z",
+          "__v": 0
+        },
+    ]
+    const [notes, setNotes] = useState(notesInitial)
     
     return (
-        <NoteContext.Provider value={{}}>
+        <NoteContext.Provider value={{notes, setNotes}}>
             {props.children}
         </NoteContext.Provider>
     )
